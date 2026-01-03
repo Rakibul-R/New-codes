b049a8c6b26441b3d1dc918d3ad61eccf093ee61
Md Rakibul Hasan <raharo5690@gmail.com>
2026-01-03 10:32:49 +0600

    Create Nested code 2.c

---
+++ Nested code 2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    for(int i=1; i<=1; i++){
+        for(int j=1; j<=5; j++){
+        printf("*");
+        }
+        printf("\n");
+       for(int j=1; j<=4; j++){
+        printf("*");
+        }
+        printf("\n");
+        for(int j=1; j<=3; j++){
+        printf("*");
+        }
+        printf("\n");
+        for(int j=1; j<=2; j++){
+        printf("*");
+        }
+        printf("\n"); 
+        for(int j=1; j<=1; j++){
+        printf("*");
+        }
+        printf("\n"); 
+    }
+ 
+    return 0;
+}
