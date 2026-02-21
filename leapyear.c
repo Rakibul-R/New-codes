ce5b327a9441fe4a48cfa169797f8a29f38439de
Md Rakibul Hasan <raharo5690@gmail.com>
2026-02-21 10:52:20 +0600

    Create leapyear.c

---
+++ leapyear.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int year;
+    printf("Enter a Year:");
+    scanf("%d",&year);
+
+    if((year%4==0 && year%100!=0)||year%400==0){
+        printf("%d is a leap year.", year);
+    } else {
+        printf("%d isn't a leap year.", year);
+    }
+
+    return 0;
+}
