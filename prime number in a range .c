c0d789400498f9c4a733ff5cf9c6daaeb5cce348
Md Rakibul Hasan <raharo5690@gmail.com>
2026-04-08 20:43:49 +0600

    Enhance prime number finder with input checks
    Added input validation for range and improved output formatting.

--- prime number in a range .c
+++ prime number in a range .c
@@ -1,25 +1,33 @@
 #include<stdio.h>
 
 int main(){
-    int n,i,find=0;
+    int n,i,find=0, prime=1;;
     printf("Enter your range (n) : ");
     scanf("%d",&n);
+
+    if(n<2){
+        printf("No prime number find in your range(%d)...Thanks you",n);
+        return 0;
+    }
+    if(n>='a'||n>='z'||n>='A'||n>='Z'||n<0){
+        printf("Invalid input...Please enter a valid number");
+        return 0;
+    }
     for( i=2; i<=n; i++){
-        int prime=1;
+        prime=1;
             for(int j=2; j<i; j++){
                 if(i%j==0){
                     prime=0;
-                    break;
+                    continue;
                 }
             }
+            if(i==2)
+            printf("\nThe prime numbers in your range(%d) is: \n",n);
          if(prime){
-            printf("%d \n",i);
+            printf("%d \t",i);
             find=1;
          }     
     }
-  if (!find)
-      {
-       printf("No prime number find in your range(%d)...Thanks you",n);
-      }
+
     return 0;
 }
