b5d40b15cf368eedc98f82d038f9d7ddb7dc4f42
Md Rakibul Hasan <raharo5690@gmail.com>
2026-04-14 20:42:37 +0600

    Add swap functions for call by value and reference
    Implement swap functions to demonstrate call by value and call by reference.

---
+++ call_a_function.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void swap(int a,int b);
+void _swap(int *a,int *b);
+
+int main() {
+    int x=5,y=8;
+
+    swap(x,y);
+    printf("x = %d, y = %d\n",x,y);
+
+    _swap(&x,&y);
+    printf("x = %d, y = %d\n",x,y);
+    return 0;
+}
+
+//call by refrence
+void _swap(int*a,int*b){
+    int t=*b;
+    *b=*a;
+    *a=t;
+    printf("a = %d, b = %d\n",*a,*b);
+}
+
+
+
+//call by value
+void swap(int a, int b){
+    int t=a;
+    a=b;
+    b=t;
+    printf("a = %d, b = %d\n",a,b );
+}
